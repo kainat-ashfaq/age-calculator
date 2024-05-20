c48420e15fa30eb24002ccdb0753e0a7e6734ba4
kainat-ashfaq <kainat.ashfaq135@gmail.com>
2024-05-20 18:45:19 +0500

    done

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var calculator = document.getElementById("cal")
+var month = document.getElementById("mon")
+var years = document.getElementById("yearss")
+var days = document.getElementById("dayss");
 function calculate(value) {
     var birthOfDate = document.getElementById("birthDate");
   var birthDate = new Date(birthOfDate.value);
   var current = new Date ()
   var age = current - birthDate
-  var finalAge = Math.floor( age / (365.25*24*60*60*1000))
-  console.log(finalAge);
+  var finalAge = Math.floor( age / (365.25*24*60*60*1000));
+years.innerHTML = finalAge
+
   var ageInMonths = Math.floor(finalAge*12)
-  console.log(ageInMonths);
+  month.innerHTML = ageInMonths
+//   console.log(ageInMonths);
   var ageInDays = Math.floor(finalAge*365.25+(current.getDate()-birthDate.getDate()))
-  console.log(ageInDays);
-  document.write(`${finalAge}`)
+  days.innerHTML = ageInDays
 }
 
